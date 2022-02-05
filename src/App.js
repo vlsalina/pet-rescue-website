80240ef17bbc75bb274de01b9480d87aa4842ccc
vlsalina <vlsalina@ucsd.edu>
2022-02-05 08:00:20 -0800

    Pet-Rescue website first commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Carousel from "./components/Carousel/Carousel";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App--box-1">
+        <Carousel />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,14 @@
+import "./Carousel.css";
+
+const Carousel = () => {
+  return (
+    <div className="carousel">
+      <div className="carousel__image carousel__image1"></div>
+      <div className="carousel__image carousel__image2"></div>
+      <div className="carousel__image carousel__image3"></div>
+      <div className="carousel__image carousel__image4"></div>
+    </div>
+  );
+};
+
+export default Carousel;
