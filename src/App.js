0b61bf6b7b4233c2f791df0bdbcf4d24e1675522
vlsalina <vlsalina@ucsd.edu>
2022-02-07 04:41:56 -0800

    Sponsors component done.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Carousel from "./components/Carousel/Carousel";
 import Learn from "./components/Learn/Learn";
 import Companions from "./components/Companions/Companions";
 import Announcements from "./components/Announcements/Announcements";
+import Sponsors from "./components/Sponsors/Sponsors";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Learn />
         <Companions />
         <Announcements />
+        <Sponsors />
       </div>
     </div>
   );
---
+++ src/components/Sponsors/Sponsors.js
@@ -0,0 +1,49 @@
+import "./Sponsors.css";
+
+const Sponsors = () => {
+  return (
+    <section className="sponsors">
+      <header>
+        <h6 className="sponsors__header">
+          Thank you to our partners whose support makes our work possible
+        </h6>
+      </header>
+      <div>
+        <ul className="sponsors__partners">
+          <li>
+            <img
+              className="sponsors__partner"
+              src={"/assets/supporters/cat-lovers.jpg"}
+            />
+          </li>
+          <li>
+            <img
+              className="sponsors__partner"
+              src={"/assets/supporters/happy-cat.jpg"}
+            />
+          </li>
+          <li>
+            <img
+              className="sponsors__partner"
+              src={"/assets/supporters/hedgehog.jpg"}
+            />
+          </li>
+          <li>
+            <img
+              className="sponsors__partner"
+              src={"/assets/supporters/pet-care.jpg"}
+            />
+          </li>
+          <li>
+            <img
+              className="sponsors__partner"
+              src={"/assets/supporters/pet-shop.jpg"}
+            />
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Sponsors;
